9be7b71880ed71e30373c68b74ced70907a8ae55
173210 <root.3.173210@live.com>
2015-06-06 10:22:39 +0900

    Fix license notices

--- tools/psp2-fixup-lib.c
+++ tools/psp2-fixup-lib.c
@@ -1,10 +1,9 @@
 /*
  * Copyright (C) 2015 PSP2SDK Project
  *
- * This library is free software; you can redistribute it and/or
+ * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License.
+ * License version 2.1 as published by the Free Software Foundation
  *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
