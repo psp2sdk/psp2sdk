1a7351e7e5262849df82bbb3c4be713064d72f4b
173210 <root.3.173210@live.com>
2015-06-16 22:04:43 +0900

    Update src/start

--- src/start/crt0.c
+++ src/start/crt0.c
@@ -15,34 +15,21 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  021100xFFFFFAEB  USA
  */
 
+#include <psp2/kernel/loadcore.h>
 #include <psp2/kernel/threadmgr.h>
+#include <psp2/moduleinfo.h>
 #include <psp2/types.h>
 #include <stddef.h>
+#include <stdint.h>
 
-__asm__ (
-"#include <psp2/export.s>\n"
-
-"PSP2_EXPORT_START\n"
-"PSP2_EXPORT_FUNC_START\n"
-"PSP2_EXPORT_FUNC 0xD632ACDB, module_start\n"
-"PSP2_EXPORT_FUNC 0xCEE8593C, module_stop\n"
-"PSP2_EXPORT_FUNC_END\n"
-
-".ifndef psp2MainThreadName\n"
-"	.globl	psp2MainThreadName\n"
-"	.section	.rodata\n"
-"psp2MainThreadName:\n"
-"	.string	\"user_main\"\n"
-"	.size	psp2MainThreadName, .-psp2MainThreadName\n"
-".endif\n"
-);
-
-extern const char psp2MainThreadName[];
+extern const char psp2MainThreadName[] __attribute__((weak));
 extern const int psp2MainThreadPriority __attribute__((weak));
 extern const SceSize psp2MainThreadStackSize __attribute__((weak));
 extern const int psp2MainThreadAttr __attribute__((weak));
 extern const int psp2MainThreadAffinity __attribute__((weak));
 extern const int psp2MainThreadOption[] __attribute__((weak));
+
+extern SceModuleInfo module_info;
 
 int main(SceSize arglen, void *argp);
 
@@ -50,7 +37,9 @@ static SceUID id;
 
 int module_start(SceSize arglen, void *argp)
 {
-	id = sceKernelCreateThread(psp2MainThreadName, main,
+	id = sceKernelCreateThread(
+		&psp2MainThreadName == NULL ? "user_main" : psp2MainThreadName,
+		main,
 		&psp2MainThreadPriority == NULL ? 32 : psp2MainThreadPriority,
 		&psp2MainThreadStackSize == NULL ? 1048576 : psp2MainThreadStackSize,
 		&psp2MainThreadAttr == NULL ? PSP2_THREAD_ATTR_USER : psp2MainThreadAttr,
@@ -63,3 +52,26 @@ int module_stop()
 {
 	return sceKernelTerminateDeleteThread(id);
 }
+
+static const uint32_t nids[3] __attribute__((section(".sceExport.rodata")))
+	= { 0x935CD196, 0x79F8E492, 0x6C2224BA };
+
+static const uint32_t ents[3] __attribute__((section(".sceExport.rodata")))
+	= { (uint32_t)module_start, (uint32_t)module_stop, (uint32_t)&module_info };
+
+static const char name[] __attribute__((section(".sceExport.rodata")))
+	= "syslib";
+
+static const SceLibraryEntryTable export
+	__attribute__((section(".sceLib.ent"), used)) = {
+		.size = sizeof(SceLibraryEntryTable),
+		.ver = 0,
+		.attr = 0x800,
+		.funcNum = 2,
+		.varNum = 1,
+		.unkNum = 0,
+		.nid = 0,
+		.name = name,
+		.nids = nids,
+		.ents = (const void **)ents
+	};
