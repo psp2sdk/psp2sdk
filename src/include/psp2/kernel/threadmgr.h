9439503e573ef0b42dce5e7d6b52b06305026407
173210 <root.3.173210@live.com>
2015-06-16 19:48:35 +0900

    Add src/include/psp2/kernel/threadmgr.h

---
+++ src/include/psp2/kernel/threadmgr.h
@@ -0,0 +1,36 @@
+/**
+ * \file
+ * \brief Header file related to thread management
+ *
+ * Copyright (C) 2015 PSP2SDK Project
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License version 2.1 as published by the Free Software Foundation
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  021100xFFFFFAEB  USA
+ */
+
+#ifndef _PSP2_KERNEL_THREADMGR_H_
+#define _PSP2_KERNEL_THREADMGR_H_
+
+#include <psp2/types.h>
+
+enum {
+	PSP2_THREAD_ATTR_USER = 0x80000000
+};
+
+SceUID sceKernelCreateThread(const char *name, void *entry,
+	unsigned int initPriority, SceSize stackSize, unsigned attr,
+	int affinity, const void *option);
+int sceKernelStartThread(SceUID thid, SceSize arglen, void *argp);
+int sceKernelTerminateDeleteThread(SceUID thid);
+
+#endif
