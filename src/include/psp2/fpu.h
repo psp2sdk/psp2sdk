61204af87adbe3eabf52aa23e2fa7a74f1637041
Sergi Granell <xerpi.g.12@gmail.com>
2015-06-19 12:53:10 +0200

    Added SceFpu header

---
+++ src/include/psp2/fpu.h
@@ -0,0 +1,21 @@
+#ifndef _PSP2_FPU
+#define _PSP2_FPU
+
+/* prototypes */
+
+float sceFpuLog2f(float x);
+float sceFpuSinf(float x);
+float sceFpuExp10f(float x);
+float sceFpuAsinf(float x);
+float sceFpuAtanf(float x);
+float sceFpuAcosf(float x);
+float sceFpuTanf(float x);
+float sceFpuLogf(float x);
+float sceFpuExp2f(float x);
+float sceFpuLog10f(float x);
+float sceFpuAtan2f(float y, float x);
+float sceFpuCosf(float x);
+float sceFpuPowf(float base, float exponent);
+float sceFpuExpf(float x);
+
+#endif
