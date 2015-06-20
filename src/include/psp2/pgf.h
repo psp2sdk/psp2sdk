14255579a16ecedd9a3f6dbf8d188cff96014b9a
173210 <root.3.173210@live.com>
2015-06-20 11:36:57 +0900

    Update src/include/psp2/pgf.h

--- src/include/psp2/pgf.h
+++ src/include/psp2/pgf.h
@@ -18,10 +18,11 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#ifndef _PSP2_PGF_
-#define _PSP2_PGF_
+#ifndef _PSP2_PGF_H_
+#define _PSP2_PGF_H_
 
 #include <psp2/types.h>
+#include <stdint.h>
 
 /* typedef */
 
@@ -40,47 +41,47 @@ enum {
 };
 
 typedef struct SceSceFontNewLibParams {
-	unsigned int *userDataAddr;
+	void *userData;
 	unsigned int numFonts;
-	unsigned int *cacheDataAddr;
+	void *cacheData;
 
 	// Driver callbacks.
-	void *(*allocFuncAddr)(void *, unsigned int);
-	void  (*freeFuncAddr )(void *, void *);
-	unsigned int *openFuncAddr;
-	unsigned int *closeFuncAddr;
-	unsigned int *readFuncAddr;
-	unsigned int *seekFuncAddr;
-	unsigned int *errorFuncAddr;
-	unsigned int *ioFinishFuncAddr;
+	void *(*allocFunc)(void *, unsigned int);
+	void  (*freeFunc)(void *, void *);
+	void *openFunc;
+	void *closeFunc;
+	void *readFunc;
+	void *seekFunc;
+	void *errorFunc;
+	void *ioFinishFunc;
 } SceFontNewLibParams;
 
 typedef enum FontFamily {
-	FONT_FAMILY_SANS_SERIF = 1,
-	FONT_FAMILY_SERIF      = 2,
+	FONT_FAMILY_SANS_SERIF	= 1,
+	FONT_FAMILY_SERIF	= 2,
 } FontFamily;
 
 typedef enum FontStyle {
-	FONT_STYLE_REGULAR     = 1,
-	FONT_STYLE_ITALIC      = 2,
-	FONT_STYLE_BOLD        = 5,
-	FONT_STYLE_BOLD_ITALIC = 6,
-	FONT_STYLE_DB          = 103, // Demi-Bold / semi-bold
+	FONT_STYLE_REGULAR	= 1,
+	FONT_STYLE_ITALIC	= 2,
+	FONT_STYLE_BOLD	= 5,
+	FONT_STYLE_BOLD_ITALIC	= 6,
+	FONT_STYLE_DB	= 103, // Demi-Bold / semi-bold
 } FontStyle;
 
 typedef enum FontLanguage {
-	FONT_LANGUAGE_JAPANESE = 1,
-	FONT_LANGUAGE_LATIN    = 2,
-	FONT_LANGUAGE_KOREAN   = 3,
-	FONT_LANGUAGE_CHINESE  = 4,
+	FONT_LANGUAGE_JAPANESE	= 1,
+	FONT_LANGUAGE_LATIN	= 2,
+	FONT_LANGUAGE_KOREAN	= 3,
+	FONT_LANGUAGE_CHINESE	= 4,
 }FontLanguage;
 
 typedef enum FontPixelFormat {
-	FONT_PIXELFORMAT_4     = 0, // 2 pixels packed in 1 byte (natural order)
-	FONT_PIXELFORMAT_4_REV = 1, // 2 pixels packed in 1 byte (reversed order)
-	FONT_PIXELFORMAT_8     = 2, // 1 pixel in 1 byte
-	FONT_PIXELFORMAT_24    = 3, // 1 pixel in 3 bytes (RGB)
-	FONT_PIXELFORMAT_32    = 4, // 1 pixel in 4 bytes (RGBA)
+	FONT_PIXELFORMAT_4	= 0, // 2 pixels packed in 1 byte (natural order)
+	FONT_PIXELFORMAT_4_REV	= 1, // 2 pixels packed in 1 byte (reversed order)
+	FONT_PIXELFORMAT_8	= 2, // 1 pixel in 1 byte
+	FONT_PIXELFORMAT_24	= 3, // 1 pixel in 3 bytes (RGB)
+	FONT_PIXELFORMAT_32	= 4, // 1 pixel in 4 bytes (RGBA)
 } FontPixelFormat;
 
 typedef struct SceFontImageRect {
@@ -100,22 +101,22 @@ typedef struct SceFontGlyphImage {
 } SceFontGlyphImage;
 
 typedef struct SceFontStyle {
-	float  fontH;
-	float  fontV;
-	float  fontHRes;
-	float  fontVRes;
-	float  fontWeight;
-	u16    fontFamily;
-	u16    fontStyle;
+	float fontH;
+	float fontV;
+	float fontHRes;
+	float fontVRes;
+	float fontWeight;
+	unsigned short fontFamily;
+	unsigned short fontStyle;
 	// Check.
-	u16    fontStyleSub;
-	u16    fontLanguage;
-	u16    fontRegion;
-	u16    fontCountry;
-	char   fontName[64];
-	char   fontFileName[64];
-	unsigned int    fontAttributes;
-	unsigned int    fontExpire;
+	unsigned short fontStyleSub;
+	unsigned short fontLanguage;
+	unsigned short fontRegion;
+	unsigned short fontCountry;
+	char fontName[64];
+	char fontFileName[64];
+	unsigned int fontAttributes;
+	unsigned int fontExpire;
 } SceFontStyle;
 
 typedef struct SceFontCharInfo {
@@ -166,14 +167,14 @@ typedef struct SceFontInfo {
 	// Bitmap dimensions.
 	short maxGlyphWidth;
 	short maxGlyphHeight;
-	unsigned int  charMapLength;   // Number of elements in the font's charmap.
-	unsigned int  shadowMapLength; // Number of elements in the font's shadow charmap.
+	unsigned int charMapLength;   // Number of elements in the font's charmap.
+	unsigned int shadowMapLength; // Number of elements in the font's shadow charmap.
 
 	// Font style (used by font comparison functions).
 	SceFontStyle fontStyle;
 
-	u8 BPP; // Font's BPP.
-	u8 pad[3];
+	uint8_t BPP; // Font's BPP.
+	uint8_t pad[3];
 } SceFontInfo;
 
 /* prototypes */
@@ -181,31 +182,61 @@ typedef struct SceFontInfo {
 SceFontLibHandle sceFontNewLib(SceFontNewLibParams *params, unsigned int *errorCode);
 int sceFontDoneLib(SceFontLibHandle libHandle);
 
-SceFontHandle sceFontOpen(SceFontLibHandle libHandle, int index, int mode, unsigned int *errorCode);
-SceFontHandle sceFontOpenUserMemory(SceFontLibHandle libHandle, void *memoryFontAddr, int memoryFontLength, unsigned int *errorCode);
-SceFontHandle sceFontOpenUserFile(SceFontLibHandle libHandle, char *fileName, int mode, unsigned int *errorCode);
+SceFontHandle sceFontOpen(SceFontLibHandle libHandle,
+	int index, int mode, unsigned int *errorCode);
+
+SceFontHandle sceFontOpenUserMemory(SceFontLibHandle libHandle,
+	void *pMemoryFont, SceSize pMemoryFontSize, unsigned int *errorCode);
+
+SceFontHandle sceFontOpenUserFile(SceFontLibHandle libHandle,
+	char *file, int mode, unsigned int *errorCode);
+
 int sceFontClose(SceFontHandle fontHandle);
 
 int sceFontGetNumFontList(SceFontLibHandle libHandle, unsigned int *errorCode);
-int sceFontFindOptimumFont(SceFontLibHandle libHandle, SceFontStyle *fontStyle, unsigned int *errorCode);
-int sceFontFindFont(SceFontLibHandle libHandle, SceFontStyle *fontStyle, unsigned int *errorCode);
+
+int sceFontFindOptimumFont(SceFontLibHandle libHandle,
+	SceFontStyle *fontStyle, unsigned int *errorCode);
+
+int sceFontFindFont(SceFontLibHandle libHandle,
+	SceFontStyle *fontStyle, unsigned int *errorCode);
+
 int sceFontGetFontInfo(SceFontHandle fontHandle, SceFontInfo *fontInfo);
-int sceFontGetFontInfoByIndexNumber(SceFontLibHandle libHandle, SceFontStyle *fontStyle, int unknown, int fontIndex);
+
+int sceFontGetFontInfoByIndexNumber(SceFontLibHandle libHandle,
+	SceFontStyle *fontStyle, int unknown, int fontIndex);
 
 int sceFontSetResolution(SceFontLibHandle libHandle, float hRes, float vRes);
 
-int sceFontGetFontList(SceFontLibHandle libHandle, SceFontStyle *fontStyle, int numFonts);
-int sceFontGetCharInfo(SceFontHandle fontHandle, u32 charCode, SceFontCharInfo *charInfo);
-int sceFontGetCharImageRect(SceFontHandle fontHandle, u32 charCode, SceFontImageRect *charRect);
-int sceFontGetCharGlyphImage(SceFontHandle fontHandle, u32 charCode, SceFontGlyphImage *glyphImage);
-int sceFontGetCharGlyphImage_Clip(SceFontHandle fontHandle, u32 charCode, SceFontGlyphImage *glyphImage, int clipXPos, int clipYPos, int clipWidth, int clipHeight);
+int sceFontGetFontList(SceFontLibHandle libHandle,
+	SceFontStyle *fontStyle, int numFonts);
 
-float sceFontPixelToPointH(SceFontLibHandle libHandle, float fontPixelsH, unsigned int *errorCode);
-float sceFontPixelToPointV(SceFontLibHandle libHandle, float fontPixelsV, unsigned int *errorCode);
-float sceFontPointToPixelH(SceFontLibHandle libHandle, float fontPointsH, unsigned int *errorCode);
-float sceFontPointToPixelV(SceFontLibHandle libHandle, float fontPointsV, unsigned int *errorCode);
+int sceFontGetCharInfo(SceFontHandle fontHandle,
+	unsigned int charCode, SceFontCharInfo *charInfo);
 
-int sceFontSetAltCharacterCode(SceFontLibHandle libHandle, u32 charCode);
+int sceFontGetCharImageRect(SceFontHandle fontHandle,
+	unsigned int charCode, SceFontImageRect *charRect);
+
+int sceFontGetCharGlyphImage(SceFontHandle fontHandle,
+	unsigned int charCode, SceFontGlyphImage *glyphImage);
+
+int sceFontGetCharGlyphImage_Clip(SceFontHandle fontHandle,
+	unsigned int charCode, SceFontGlyphImage *glyphImage,
+	int clipXPos, int clipYPos, int clipWidth, int clipHeight);
+
+float sceFontPixelToPointH(SceFontLibHandle libHandle,
+	float fontPixelsH, unsigned int *errorCode);
+
+float sceFontPixelToPointV(SceFontLibHandle libHandle,
+	float fontPixelsV, unsigned int *errorCode);
+
+float sceFontPointToPixelH(SceFontLibHandle libHandle,
+	float fontPointsH, unsigned int *errorCode);
+
+float sceFontPointToPixelV(SceFontLibHandle libHandle,
+	float fontPointsV, unsigned int *errorCode);
+
+int sceFontSetAltCharacterCode(SceFontLibHandle libHandle, unsigned int charCode);
 
 int sceFontFlush(SceFontHandle fontHandle);
 
