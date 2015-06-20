1cc3010033197668789d53630e75c57e50121f21
Sergi Granell <xerpi.g.12@gmail.com>
2015-06-20 16:25:46 +0200

    Change "Source Code Form" to "Program" in psp2-fixup.c

--- tools/psp2-fixup/psp2-fixup.c
+++ tools/psp2-fixup/psp2-fixup.c
@@ -20,7 +20,7 @@ int main(int argc, char *argv[])
 		fprintf(stderr, "Usage: %s <INPUT> <OUTPUT>\n\n"
 "psp2-fixup (" PACKAGE_NAME ") " PACKAGE_VERSION "\n"
 "Copyright (C) 2015  PSP2SDK Project\n"
-"This Source Code Form is subject to the terms of the Mozilla Public\n"
+"This Program is subject to the terms of the Mozilla Public\n"
 "License, v. 2.0. If a copy of the MPL was not distributed with this\n"
 "file, You can obtain one at http://mozilla.org/MPL/2.0/.\n", argv[0]);
 		return EINVAL;
