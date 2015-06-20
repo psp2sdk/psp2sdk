9ab6d19638bee79b5440ef2ef1635d0661933b97
173210 <root.3.173210@live.com>
2015-06-20 12:01:18 +0900

    Update src/include/psp2/system_param.h

--- src/include/psp2/system_param.h
+++ src/include/psp2/system_param.h
@@ -21,55 +21,81 @@
 #ifndef _PSP2_SYSTEM_PARAM_H_
 #define _PSP2_SYSTEM_PARAM_H_
 
-#include <psp2/types.h>
-
 /* System param id */
-typedef uint32_t SystemParamId;
-#define SYSTEM_PARAM_ID_LANG	(1) //!< Language settings
-#define SYSTEM_PARAM_ID_ENTER_BUTTON	(2) //!< Enter button assignment
-#define SYSTEM_PARAM_ID_USERNAME	(3) //!< Username string
-#define SYSTEM_PARAM_ID_DATE_FORMAT	(4) //!< Date format
-#define SYSTEM_PARAM_ID_TIME_FORMAT	(5) //!< Time format
-#define SYSTEM_PARAM_ID_TIME_ZONE	(6) //!< Time zone
-#define SYSTEM_PARAM_ID_DAYLIGHT_SAVINGS	(7) //!< Daylight savings time (0=Disabled 1=Enabled)
+
+enum {
+	//!< Language settings
+	PSP2_SYSTEM_PARAM_ID_LANG = 1,
+
+	//!< Enter button assignment
+	PSP2_SYSTEM_PARAM_ID_ENTER_BUTTON,
+
+	//!< Username string
+	PSP2_SYSTEM_PARAM_ID_USERNAME,
+
+	//!< Date format
+	PSP2_SYSTEM_PARAM_ID_DATE_FORMAT,
+
+	//!< Time format
+	PSP2_SYSTEM_PARAM_ID_TIME_FORMAT,
+
+	//!< Time zone
+	PSP2_SYSTEM_PARAM_ID_TIME_ZONE,
+
+	//!< Daylight savings time (0 = Disabled, 1 = Enabled)
+	PSP2_SYSTEM_PARAM_ID_DAYLIGHT_SAVINGS
+};
 
 /* Language settings */
-typedef int32_t SystemParamLang;
-#define SYSTEM_PARAM_LANG_JAPANESE	(0)
-#define SYSTEM_PARAM_LANG_ENGLISH_US	(1)
-#define SYSTEM_PARAM_LANG_FRENCH	(2)
-#define SYSTEM_PARAM_LANG_SPANISH	(3)
-#define SYSTEM_PARAM_LANG_GERMAN	(4)
-#define SYSTEM_PARAM_LANG_ITALIAN	(5)
-#define SYSTEM_PARAM_LANG_DUTCH	(6)
-#define SYSTEM_PARAM_LANG_PORTUGUESE	(7)
-#define SYSTEM_PARAM_LANG_RUSSIAN	(8)
-#define SYSTEM_PARAM_LANG_KOREAN	(9)
-#define SYSTEM_PARAM_LANG_CHINESE_T	(10) //!< Traditional Chinese
-#define SYSTEM_PARAM_LANG_CHINESE_S	(11) //!< Simplified Chinese
-#define SYSTEM_PARAM_LANG_FINNISH	(12)
-#define SYSTEM_PARAM_LANG_SWEDISH	(13)
-#define SYSTEM_PARAM_LANG_DANISH	(14)
-#define SYSTEM_PARAM_LANG_NORWEGIAN	(15)
-#define SYSTEM_PARAM_LANG_POLISH	(16)
-#define SYSTEM_PARAM_LANG_POLISH_BR	(17) //!< Brazil Portuguese
-#define SYSTEM_PARAM_LANG_ENGLISH_GB	(18)
+enum {
+	PSP2_SYSTEM_PARAM_LANG_JAPANESE,
+	PSP2_SYSTEM_PARAM_LANG_ENGLISH_US,
+	PSP2_SYSTEM_PARAM_LANG_FRENCH,
+	PSP2_SYSTEM_PARAM_LANG_SPANISH,
+	PSP2_SYSTEM_PARAM_LANG_GERMAN,
+	PSP2_SYSTEM_PARAM_LANG_ITALIAN,
+	PSP2_SYSTEM_PARAM_LANG_DUTCH,
+	PSP2_SYSTEM_PARAM_LANG_PORTUGUESE,
+	PSP2_SYSTEM_PARAM_LANG_RUSSIAN,
+	PSP2_SYSTEM_PARAM_LANG_KOREAN,
+
+	//! Traditional Chinese
+	PSP2_SYSTEM_PARAM_LANG_CHINESE_T,
+
+	//! Simplified Chinese
+	PSP2_SYSTEM_PARAM_LANG_CHINESE_S,
+	PSP2_SYSTEM_PARAM_LANG_FINNISH,
+	PSP2_SYSTEM_PARAM_LANG_SWEDISH,
+	PSP2_SYSTEM_PARAM_LANG_DANISH,
+	PSP2_SYSTEM_PARAM_LANG_NORWEGIAN,
+	PSP2_SYSTEM_PARAM_LANG_POLISH,
+
+	//! Brazil Portuguese
+	PSP2_SYSTEM_PARAM_LANG_POLISH_BR,
+
+	PSP2_SYSTEM_PARAM_LANG_ENGLISH_GB
+};
 
 /* Assignment of enter button */
-typedef int32_t SystemParamEnterButton;
-#define SYSTEM_PARAM_ENTER_BUTTON_CIRCLE	(0)
-#define SYSTEM_PARAM_ENTER_BUTTON_CROSS (1)
+enum {
+	PSP2_SYSTEM_PARAM_ENTER_BUTTON_CIRCLE,
+	PSP2_SYSTEM_PARAM_ENTER_BUTTON_CROSS
+};
 
 /* Username */
 #define SYSTEM_PARAM_USERNAME_MAXSIZE	(17) //!< Max size of username
 
 /* Date display format */
-#define SYSTEM_PARAM_DATE_FORMAT_YYYYMMDD	(0) //!< Year/Month/Day
-#define SYSTEM_PARAM_DATE_FORMAT_DDMMYYYY	(1) //!< Day/Month/Year
-#define SYSTEM_PARAM_DATE_FORMAT_MMDDYYYY	(2) //!< Month/Day/Year
+enum {
+	PSP2_SYSTEM_PARAM_DATE_FORMAT_YYYYMMDD, //!< Year/Month/Day
+	PSP2_SYSTEM_PARAM_DATE_FORMAT_DDMMYYYY, //!< Day/Month/Year
+	PSP2_SYSTEM_PARAM_DATE_FORMAT_MMDDYYYY //!< Month/Day/Year
+};
 
 /* Time display format */
-#define SYSTEM_PARAM_TIME_FORMAT_12HR	(0) //!< 12-hour clock
-#define SYSTEM_PARAM_TIME_FORMAT_24HR	(1) //!< 24-hour clock
+enum {
+	SYSTEM_PARAM_TIME_FORMAT_12HR, //!< 12-hour clock
+	SYSTEM_PARAM_TIME_FORMAT_24HR //!< 24-hour clock
+};
 
 #endif
