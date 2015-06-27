d4831302b5b69c0d84c9131b09a9e8a17d87b2e1
Ryp <ryp.sqrt@gmail.com>
2015-06-27 23:42:24 +0100

    Fix install tree

--- src/cmake/arm-none-eabi-psp2.cmake
+++ src/cmake/arm-none-eabi-psp2.cmake
@@ -8,24 +8,30 @@ include(CMakeForceCompiler)
 
 set(CMAKE_SYSTEM_NAME Generic)
 set(CMAKE_SYSTEM_PROCESSOR cortex-a9)
+set(CMAKE_CROSSCOMPILING ON)
 CMAKE_FORCE_C_COMPILER(arm-none-eabi-gcc GNU)
 CMAKE_FORCE_CXX_COMPILER(arm-none-eabi-g++ GNU)
 
+# Do a no-op access on the CMAKE_TOOLCHAIN_FILE variable
+# so that CMake will not issue a warning on it being unused.
+if (CMAKE_TOOLCHAIN_FILE)
+endif()
+
 # Find the target environment prefix
 execute_process(COMMAND ${CMAKE_C_COMPILER} -print-file-name=libc.a
-    OUTPUT_VARIABLE CMAKE_INSTALL_PREFIX OUTPUT_STRIP_TRAILING_WHITESPACE)
+    OUTPUT_VARIABLE TOOLCHAIN_INSTALL_PREFIX OUTPUT_STRIP_TRAILING_WHITESPACE)
 
 # Strip the filename off
-get_filename_component(CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}" PATH)
+get_filename_component(TOOLCHAIN_INSTALL_PREFIX "${TOOLCHAIN_INSTALL_PREFIX}" PATH)
 # Then find the canonical path to the directory one up from there
-get_filename_component(CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}/.." REALPATH)
-set(CMAKE_INSTALL_PREFIX  ${CMAKE_INSTALL_PREFIX} CACHE FILEPATH
+get_filename_component(TOOLCHAIN_INSTALL_PREFIX "${TOOLCHAIN_INSTALL_PREFIX}/.." REALPATH)
+set(TOOLCHAIN_INSTALL_PREFIX  ${TOOLCHAIN_INSTALL_PREFIX} CACHE FILEPATH
     "Install path prefix, prepended onto install directories.")
 
 message(STATUS "Cross-compiling with gcc-arm-none-eabi toolchain")
-message(STATUS "Toolchain prefix: ${CMAKE_INSTALL_PREFIX}")
+message(STATUS "Toolchain prefix: ${TOOLCHAIN_INSTALL_PREFIX}")
 
-set(CMAKE_FIND_ROOT_PATH ${CMAKE_INSTALL_PREFIX})
+set(CMAKE_FIND_ROOT_PATH ${TOOLCHAIN_INSTALL_PREFIX})
 
 # adjust the default behaviour of the FIND_XXX() commands:
 # search headers and libraries in the target environment, search
@@ -33,6 +39,3 @@ set(CMAKE_FIND_ROOT_PATH ${CMAKE_INSTALL_PREFIX})
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
-
-# -fno-common -ffunction-sections -fdata-sections
-# -mcpu=cortex-m4 -march=armv7e-m -mthumb -mfloat-abi=hard -mfpu=fpv4-sp-d16
